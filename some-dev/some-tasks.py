4916fbb5b8a2e602f49537fce7a71b83b50d3845
Anton Svirydau <unskillton@gmail.com>
2024-08-14 21:27:25 +0300

    Update some-tasks.py

--- some-dev/some-tasks.py
+++ some-dev/some-tasks.py
@@ -1,34 +1,12 @@
-"""
-def some_reverse(number_of_requests):
-
-    print(f'Enter {number_of_requests} numbers, no more than 5 digits each')
-
-    for i in range(number_of_requests):
-        some_number = int(input())
-        try:
-            if len(str(some_number)) > 5:
-                raise Exception('Incorrect number')
-            reverse_number = int(str(some_number)[::-1])
-            print(reverse_number)
-        except ValueError:
-            print('Incorrect data')
-        except Exception as e:
-            print(e)
-        print('The code is correct')
-
-
-some_reverse(3)
-"""
-
-
 def number_reverse():
 
     print('Enter number, no more than 5 digits')
     input_number = int(input())
+    str_number = str(input_number)
     try:
-        if len(str(input_number)) > 5:
+        if len(str_number) > 5:
             raise Exception('Incorrect number')
-        reverse_number = int(str(input_number)[::-1])
+        reverse_number = int(str_number[::-1])
         print(reverse_number)
     except ValueError:
         print('Incorrect data')
