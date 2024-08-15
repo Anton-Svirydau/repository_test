61f2ad5608f2c71bc4aabaeeb4be43cd5872e8d0
Anton Svirydau <unskillton@gmail.com>
2024-08-15 18:19:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-
+"""
 interest_rate = 1.01
 initial_debt = 1000
 
@@ -30,3 +30,74 @@ def reverse_sequence():
 
 
 reverse_sequence()
+"""
+
+"""
+def selection_sort(self, unsorted, n):
+  
+  for i in range(0, n):
+
+    current_min = unsorted[i]
+
+    min_index = i
+
+    for j in range(i, n):
+
+      if unsorted[j] < current_min:
+        current_min = unsorted[j]
+        min_index = j
+
+    swap(unsorted, i, min_index)
+"""
+
+"""
+def bubble_sort(self, unsorted, n):
+    for i in range(0, n - 1):
+
+        swapped = False
+
+    for j in range(0, n - 1 - i):
+
+        if unsorted[j].value > unsorted[j + 1].value:
+            swap(unsorted, j, j + 1)
+            swapped = True
+"""
+
+
+def divide(self, unsorted, lower, upper):
+
+    if upper <= lower:
+        return
+
+    mid = (lower + upper) // 2
+
+    divide(unsorted, lower, mid)
+    divide(unsorted, mid + 1, upper)
+
+    merge(unsorted, lower, mid, mid + 1, upper)
+
+
+def merge(unsorted, l_lower, l_upper, r_lower, r_upper):
+
+    i, j = l_lower, r_lower
+
+    temp = []
+
+    while i <= l_upper and j <= r_upper:
+
+        if unsorted[i].value <= unsorted[j].value:
+            temp.append(unsorted[i])
+            i += 1
+        else:
+            temp.append(unsorted[j])
+            j += 1
+
+    while i <= l_upper:
+        temp.append(unsorted[i])
+        i += 1
+    while j <= r_upper:
+        temp.append(unsorted[j])
+        j += 1
+
+    for y, k in enumerate(range(l_lower, r_upper + 1)):
+        unsorted[k] = temp[y]
