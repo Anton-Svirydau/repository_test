008c3a6543eb26d0923a13d1408ba2736668eaae
Anton Svirydau <unskillton@gmail.com>
2024-08-19 20:29:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -331,7 +331,7 @@ print(*a, sep='+', end='')
 print('=' + str(sum(a)))
 '''
 
-
+'''
 def arithmetic(a, b, operation):
     if operation == '+':
         return a + b
@@ -354,3 +354,17 @@ print(arithmetic(10, 5, '*'))
 print(arithmetic(10, 5, '/'))
 print(arithmetic(10, 0, '/'))
 print(arithmetic(10, 5, '^'))
+'''
+
+
+def is_year_leap(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+
+print(is_year_leap(2020))
+print(is_year_leap(1900))
+print(is_year_leap(2000))
+print(is_year_leap(2021))
