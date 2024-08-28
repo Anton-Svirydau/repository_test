e0a4c3444208a5391d630ed57e19fb83063f26d2
Anton Svirydau <unskillton@gmail.com>
2024-08-28 19:52:00 +0300

    Update some course.py

--- some course.py
+++ some course.py
@@ -32,3 +32,33 @@ print(x, y)
 variable_1, variable_2 = 1, "Hype"
 print(type(variable_1), type(variable_2))
 '''
+
+'''
+my_integer1 = 10
+my_integer2 = 5
+my_float1 = 20.5
+my_float2 = 17.5
+
+summary1 = my_integer1 + my_integer2
+summary2 = my_float1 + my_float2
+multiplication1 = my_integer1 * my_integer2
+division1 = my_integer1 / my_integer2
+division2 = my_integer1 // my_integer2
+division3 = my_integer1 % my_integer2
+
+print(my_integer2 * my_integer2 * my_integer2)
+print(my_integer2 ** 3)
+print(my_integer1 + my_float1)
+
+print(summary1, summary2, multiplication1, division1, division2, division3)
+
+my_float = float(my_integer1)
+print(my_float)
+
+my_float3 = 1.999
+my_integer3 = int(my_float3)
+my_integer4 = round(my_float3)
+
+print(my_integer3, my_integer4)
+'''
+
