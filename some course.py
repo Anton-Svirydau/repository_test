32543104f82a796a3970b000a7995d3d990f70dc
Anton Svirydau <unskillton@gmail.com>
2024-08-28 17:49:18 +0300

    Update some course.py

--- some course.py
+++ some course.py
@@ -11,3 +11,24 @@ print(1)
 name = input('Enter your name: ')
 print(name)
 '''
+
+'''
+name, age = 'Alice', 25
+print(name, age)
+
+other_name = name
+print(other_name)
+'''
+
+'''
+x = 3
+y = 4
+
+x, y = y, x
+print(x, y)
+'''
+
+'''
+variable_1, variable_2 = 1, "Hype"
+print(type(variable_1), type(variable_2))
+'''
