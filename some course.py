0700c58bbbeb649783d348195fc78505ba226263
Anton Svirydau <unskillton@gmail.com>
2024-08-28 17:40:26 +0300

    Create some course.py

---
+++ some course.py
@@ -0,0 +1,13 @@
+# print('Hello World')
+
+"""
+print('Hello', 'World', end=' ', sep='')
+print('Hello', 'World')
+print(1)
+"""
+# input('Please enter something')
+
+'''
+name = input('Enter your name: ')
+print(name)
+'''
