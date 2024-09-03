a513d74b22171192abb7e324762c999314523c0b
Anton Svirydau <unskillton@gmail.com>
2024-09-03 19:20:32 +0300

    Create some course 2.py

---
+++ some course 2.py
@@ -0,0 +1,16 @@
+# list
+
+""""""
+
+'''
+empty_list = list()
+fruits = ["apple", "banana", "cherry"]
+my_list = ["apple", 1, 1.5, True, [1, 2, 3]]
+print(fruits, len(fruits), my_list)
+'''
+
+my_list1 = [1, 2, 3]
+my_list2 = [1, 3, 2]
+my_list3 = [1, 2, 3]
+
+print(my_list1 == my_list2, my_list1 == my_list3)
