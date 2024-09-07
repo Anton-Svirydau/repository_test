28a7d5d44f967bada24b40a72e11c65f3f8d8b29
Anton Svirydau <unskillton@gmail.com>
2024-09-07 10:32:46 +0300

    Update some course 2.py

--- some course 2.py
+++ some course 2.py
@@ -1,5 +1,5 @@
-# list, index, for, function, scope
-""""""
+# list, index, for, function, scope, while
+import random
 
 '''
 empty_list = list()
@@ -299,6 +299,7 @@ def get_diff_from_comfortable_temperature(*, temperature: int) -> int:
 print(get_diff_from_comfortable_temperature(temperature=20))
 '''
 
+'''
 DEFAULT_LEVEL_EXPERIENCE = 200
 
 
@@ -314,3 +315,97 @@ def is_leveled_up(*, current_experience: int, gained_experience: int) -> bool:
 
 print(is_leveled_up(current_experience=150, gained_experience=60))
 print(is_leveled_up(current_experience=10, gained_experience=30))
+'''
+
+'''
+counter = 1
+
+while counter <= 5:
+    print(f"Counter is: {counter}")
+    counter += 1
+'''
+
+'''
+my_list = [0, 1, 2]
+
+while my_list:
+    element = my_list.pop()
+    print(f"element: {element}")
+
+print(my_list)
+'''
+
+'''
+while True:
+    answer = input("Enter a number: ")
+    if answer == "quit":
+        break
+    print(f"You entered: {answer}")
+'''
+
+HEADS = "heads"
+TAILS = "tails"
+COIN_VALUES = [HEADS, TAILS]
+
+
+def flip_coin():
+    return random.choice(COIN_VALUES)
+
+
+def play_martingale(*, starting_funds: int, min_bet: int, max_bet: int) -> int:
+    steps_to_lose = 0
+    current_funds = starting_funds
+    current_bet = min_bet
+
+    while current_funds > 0:
+        # print("============")
+        steps_to_lose += 1
+        current_funds -= current_bet
+        # print(f"{current_funds=}, {current_bet=}")
+        flipped_coin_value = flip_coin()
+        if flipped_coin_value == HEADS:
+            win = current_bet * 2
+            # print(f"{win=}")
+            current_funds += win
+            current_bet = min_bet
+        else:
+            # print("lose")
+            current_bet *= 2
+            if current_bet > max_bet:
+                current_bet = min_bet
+            if current_bet > current_funds:
+                current_bet = current_funds
+
+    return steps_to_lose
+
+
+# print(play_martingale(starting_funds=100, min_bet=1, max_bet=100))
+
+# for n players
+def simulate_martingale_for_n_players(
+        *,
+        starting_funds: int,
+        min_bet: int,
+        max_bet: int,
+        n_games: int
+) -> float:
+    total_steps_to_lose = 0
+    for i in range(n_games):
+        steps_to_lose = play_martingale(
+            starting_funds=starting_funds,
+            min_bet=min_bet,
+            max_bet=max_bet
+        )
+        total_steps_to_lose += steps_to_lose
+
+    return total_steps_to_lose / n_games
+
+
+print(
+    simulate_martingale_for_n_players(
+        n_games=10,
+        starting_funds=1000,
+        min_bet=1,
+        max_bet=100
+    )
+)
