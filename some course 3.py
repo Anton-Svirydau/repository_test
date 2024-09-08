2beca4b59509f8de396ac974b0374ac3a5779768
Anton Svirydau <unskillton@gmail.com>
2024-09-08 18:42:31 +0300

    Update some course 3.py

--- some course 3.py
+++ some course 3.py
@@ -115,15 +115,7 @@ person = person | additional_person_info
 print(person)
 '''
 
-students = [
-    {"name": "John", "surname": "Doe", "grades": [5, 5, 4, 4]},
-    {"name": "Jane", "surname": "Doe", "grades": [4, 3, 4, 3, 5]},
-    {"name": "Bill", "surname": "Gates", "grades": [5, 5, 5, 3]},
-    {"name": "Steve", "surname": "Jobs", "grades": [3, 5, 4, 3, 3, 5]},
-    {"name": "Guido", "surname": "Van Rossum", "grades": [5, 3, 5, 4, 5, 5, 3, 5]},
-    {"name": "Elon", "surname": "Musk", "grades": None}
-]
-
+"""
 average_marks = []
 best_students = []
 
@@ -145,3 +137,33 @@ for a in range(len(average_marks)):
         best_students.append(students[a])
 
 print(best_students)
+"""
+
+students_1 = [
+    {"name": "John", "surname": "Doe", "grades": [5, 5, 4, 4]},
+    {"name": "Jane", "surname": "Doe", "grades": [4, 3, 4, 3, 5]},
+    {"name": "Bill", "surname": "Gates", "grades": [5, 5, 5, 3]},
+    {"name": "Steve", "surname": "Jobs", "grades": [3, 5, 4, 3, 3, 5]},
+    {"name": "Guido", "surname": "Van Rossum", "grades": [5, 3, 5, 4, 5, 5, 3, 5]},
+    {"name": "Elon", "surname": "Musk", "grades": None}
+]
+
+
+def get_best_students(*, students: list[dict]) -> list[dict]:
+    best_students = []
+    best_average_grade = 0
+    for student in students:
+        grades = student["grades"]
+        if grades is None:
+            average_grade = 0
+        else:
+            average_grade = sum(grades) / len(grades)
+        if average_grade > best_average_grade:
+            best_average_grade = average_grade
+            best_students = [student]
+        elif average_grade == best_average_grade:
+            best_students.append(student)
+    return best_students
+
+
+print(get_best_students(students=students_1))
