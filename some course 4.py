b9ee25d3a83913962cc1dba5a2347ba38dbb9101
Anton Svirydau <unskillton@gmail.com>
2024-09-09 21:17:54 +0300

    Create some course 4.py

---
+++ some course 4.py
@@ -0,0 +1,14 @@
+""""""
+import random
+from math_operations import add
+from math_operations import subtract as sub
+
+my_int = 1
+my_list = [1, 2, 3]
+
+print(random.choice(my_list))
+print(dir(random))
+print(globals().keys())
+
+print(add(1, 2))
+print(sub(2, 1))
