4ce7919b38dc86631cbd11cf13942af12efd9c28
Anton Svirydau <unskillton@gmail.com>
2024-09-10 13:28:08 +0300

    Update list_comprehension.py

--- Some course/list_comprehension.py
+++ Some course/list_comprehension.py
@@ -43,4 +43,26 @@ labelled_numbers = ["even" if num % 2 == 0 else "odd" for num in numbers]
 print(labelled_numbers)
 '''
 
+'''
+square_dict = {x: x ** 2 for x in range(10)}
 
+print(square_dict)
+'''
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+transpose_matrix = []
+
+for i in range(len(matrix)):
+    transpose_row = []
+    for row in matrix:
+        transpose_row.append(row[i])
+    transpose_matrix.append(transpose_row)
+
+# transpose_matrix = [[row[i] for row in matrix] for i in range(len(matrix))]
+
+print(transpose_matrix)
