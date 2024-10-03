205488a4c3271be4ec10bf57dd3dbbd323770563
Anton Svirydau <unskillton@gmail.com>
2024-10-03 17:11:16 +0300

    Update telegram_bot.py

--- tg_bot/pythonProject1/telegram_bot.py
+++ tg_bot/pythonProject1/telegram_bot.py
@@ -1,44 +1,14 @@
 from telebot import TeleBot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-import requests
 
-TOKEN = ""  # register at https://telegram.me/BotFather to get TOKEN
-CRYPTO_NAME_TO_TICKER = {
-    "Bitcoin": "BTCUSDT",
-    "Ethereum": "ETHUSDT",
-    "Doge": "DOGEUSDT"
-}
-
+TOKEN = "7519131220:AAEUsqn2XovEMu8b5pEc197V-SBQAK4M2Wo"
 
 bot = TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
-    markup = ReplyKeyboardMarkup(row_width=3)
-    for crypto_name in CRYPTO_NAME_TO_TICKER.keys():
-        item_button = KeyboardButton(crypto_name)
-        markup.add(item_button)
-    bot.send_message(message.chat.id, "Choose a crypto", reply_markup=markup)
-
-
-@bot.message_handler(func=lambda message: message.text in CRYPTO_NAME_TO_TICKER.keys())
-def send_price(message):
-    crypto_name = message.text
-    ticker = CRYPTO_NAME_TO_TICKER[crypto_name]
-    price = get_price_by_ticker(ticker=ticker)
-    bot.send_message(message.chat.id, f"Price of {crypto_name} to USDT is {price}")
-
-
-def get_price_by_ticker(*, ticker: str) -> float:
-    endpoint = "https://api.binance.com/api/v3/ticker/price"
-    params = {
-        'symbol': ticker,
-    }
-    response = requests.get(endpoint, params=params)
-    data = response.json()
-    price = round(float(data["price"]), 2)
-    return price
+    print(message)
+    bot.reply_to(message, "Howdy, how are doing?")
 
 
 bot.infinity_polling()
