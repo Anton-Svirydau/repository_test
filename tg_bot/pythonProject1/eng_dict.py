a4fac8aa0ca36328ce67a5af1634d3f7bb460ef4
Anton Svirydau <unskillton@gmail.com>
2024-10-03 20:10:48 +0300

    Update eng_dict.py

--- tg_bot/pythonProject1/eng_dict.py
+++ tg_bot/pythonProject1/eng_dict.py
@@ -1,24 +1,41 @@
 import random
 
-# Пример словаря
-dictionary = {
-    "apple": "яблоко",
-    "banana": "банан",
-    "grape": "виноград",
-    "orange": "апельсин"
+# # Пример словаря
+# dictionary = {
+#     "apple": "яблоко",
+#     "banana": "банан",
+#     "grape": "виноград",
+#     "orange": "апельсин"
+# }
+#
+# # Получаем случайный ключ из словаря
+# random_key = random.choice(list(dictionary.keys()))
+#
+# # Выводим случайный ключ
+# print(f"Переведите слово: {random_key}")
+#
+# # Получаем ввод пользователя
+# user_input = input("Ваш ответ: ")
+#
+# # Проверяем ответ пользователя
+# if user_input.lower() == dictionary[random_key].lower():
+#     print("Правильно")
+# else:
+#     print(f"Неправильно, правильный ответ: {dictionary[random_key]}")
+
+Lesson_1 = {
+    "и": "and",
+    "яблоко": "apple",
+    "лампа": "lamp",
+    "позволять, допускать": "let",
+    "делать, составлять": "make",
+    "карта": "map",
+    "меня, мне": "me",
+    "перо, ручка": "pen",
+    "план": "plan",
+    "стол, таблица": "table",
+    "брать, взять": "take",
+    "сказать": "tell",
+    "тот, та, то": "that",
+    "им, их": "them"
 }
-
-# Получаем случайный ключ из словаря
-random_key = random.choice(list(dictionary.keys()))
-
-# Выводим случайный ключ
-print(f"Переведите слово: {random_key}")
-
-# Получаем ввод пользователя
-user_input = input("Ваш ответ: ")
-
-# Проверяем ответ пользователя
-if user_input.lower() == dictionary[random_key].lower():
-    print("Правильно")
-else:
-    print(f"Неправильно, правильный ответ: {dictionary[random_key]}")
