1c55738b523e906b5c01622b5375a415b73da4b7
Anton Svirydau <unskillton@gmail.com>
2024-10-03 18:40:46 +0300

    Update telegram_bot.py

--- tg_bot/pythonProject1/telegram_bot.py
+++ tg_bot/pythonProject1/telegram_bot.py
@@ -1,20 +1,23 @@
 from telebot import TeleBot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 TOKEN = "7519131220:AAEUsqn2XovEMu8b5pEc197V-SBQAK4M2Wo"
+dict_eng = {
+    "Words": "Word",
+    "Tests": "Tests"
+}
+
 
 bot = TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=["start", "help"])
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
-    print(message)
-    bot.reply_to(message, "Howdy, how are doing?")
-
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    print(message)
-    bot.reply_to(message, message.text)
+    markup = ReplyKeyboardMarkup(row_width=2)
+    for eng in dict_eng.keys():
+        item_button = KeyboardButton(eng)
+        markup.add(item_button)
+    bot.send_message(message.chat.id, "Choose one", reply_markup=markup)
 
 
 bot.infinity_polling()
