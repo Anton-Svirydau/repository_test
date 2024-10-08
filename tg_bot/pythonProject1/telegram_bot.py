c54cdeb7f38a2517f6725e7cfc29b20ad9ccfc91
Anton Svirydau <unskillton@gmail.com>
2024-10-08 23:14:21 +0300

    Update telegram_bot.py

--- tg_bot/pythonProject1/telegram_bot.py
+++ tg_bot/pythonProject1/telegram_bot.py
@@ -8,154 +8,6 @@ TOKEN = "7519131220:AAEUsqn2XovEMu8b5pEc197V-SBQAK4M2Wo"
 
 bot = TeleBot(TOKEN)
 
-lesson_1 = {
-    "и": "and",
-    "яблоко": "apple",
-    "лампа": "lamp",
-    "позволять, допускать": "let",
-    "делать, составлять": "make",
-    "карта": "map",
-    "меня, мне": "me",
-    "перо, ручка": "pen",
-    "план": "plan",
-    "стол, таблица": "table",
-    "брать, взять": "take",
-    "сказать": "tell",
-    "тот, та, то": "that",
-    "им, их": "them"
-}
-
-lesson_2 = {
-    "все, всё": "all",
-    "портфель, мешок": "bag",
-    "чёрный": "black",
-    "книга": "book",
-    "ящик, коробка": "box",
-    "ребёнок": "child",
-    "дети": "children",
-    "друг": "friend",
-    "давать": "give",
-    "хороший": "good",
-    "его": "his",
-    "в": "in",
-    "человек, мужчина": "man",
-    "мужчины": "men",
-    "мой": "my",
-    "заметка, запись": "note",
-    "тетрадь, записная книжка": "notebook",
-    "на": "on",
-    "открывать": "open",
-    "карандаш": "pencil",
-    "класть, положить": "put",
-    "видеть": "see",
-    "учить(ся), изучать": "study",
-    "хорошо": "well",
-    "женщина": "woman",
-    "женщины": "women"
-}
-
-lesson_3 = {
-    "в, на (на вопрос где?)": "at",
-    "потому что": "because",
-    "низ": "bottom",
-    "восток": "east",
-    "находить": "find",
-    "знать": "know",
-    "большой": "large",
-    "левый, налево": "left",
-    "слева": "on the left",
-    "смотреть": "look",
-    "означать, значить": "mean",
-    "север": "north",
-    "наш": "our",
-    "полюс": "pole",
-    "читать": "read",
-    "правый, право": "right",
-    "справа": "on the right",
-    "море": "sea",
-    "сторона": "side",
-    "юг": "south",
-    "текст": "text",
-    "верх": "top",
-    "запад": "west",
-    "белый": "white",
-    "слово": "word",
-    "писать": "write",
-    "ваш, твой": "your"
-}
-
-lesson_4 = {
-    "оба": "both",
-    "мост": "bridge",
-    "здание": "building",
-    "называть, звать": "call",
-    "город": "city",
-    "часы": "clock",
-    "соединять": "connect",
-    "изогнутый": "curved",
-    "конец": "end",
-    "перед, передняя сторона, фасад": "front",
-    "рука": "hand",
-    "больница": "hospital",
-    "час": "hour",
-    "дом": "house",
-    "письмо, буква": "letter",
-    "слушать": "listen",
-    "жить": "live",
-    "близко, близ": "near",
-    "почти, около": "nearly",
-    "ночь": "night",
-    "океан": "ocean",
-    "другой": "other",
-    "часть": "part",
-    "лицо, человек": "person",
-    "река": "river",
-    "квадратный": "square",
-    "бить": "strike",
-    "башня": "tower",
-    "очень": "very",
-    "широкий": "wide",
-    "с": "with"
-}
-
-lesson_5 = {
-    "воздух": "air",
-    "почти, едва не": "almost",
-    "любой": "any",
-    "док": "dock",
-    "портовый рабочий": "docker",
-    "каждый": "each",
-    "летать": "fly",
-    "для": "for",
-    "от, из": "from",
-    "ворота, врата": "gate",
-    "уходить, идти, уезжать, направляться": "go",
-    "добрый": "kind",
-    "род, сорт, класс": "kind",
-    "линия": "line",
-    "многие": "many",
-    "рот, устье (реки)": "mouth",
-    "только": "only",
-    "или": "or",
-    "место": "place",
-    "железная дорога": "railway",
-    "судно, корабль": "ship",
-    "маленький": "small",
-    "особый, специальный": "special",
-    "там, туда": "there",
-    "переводить": "translate",
-    "затем": "then",
-    "вверх": "up",
-    "хотеть, желать": "wish",
-    "когда": "when",
-    "который": "which",
-    "работа, работать": "work",
-    "рабочий, работник": "worker",
-    "мир": "world"
-}
-
-dict_list = [lesson_1, lesson_2, lesson_3, lesson_4, lesson_5]
-
 
 # def words_test():
 #     user_input = ""
@@ -197,9 +49,9 @@ def initial_choice(message):
         random_word, random_answer = test_word()
         global global_random_answer
         global_random_answer = random_answer
-        answer = f"Translate: {random_word}"
+        request_answer = f"Translate: {random_word}"
         markup = types.ReplyKeyboardRemove()
-        answer_to_user = bot.send_message(message.chat.id, answer, reply_markup=markup)
+        answer_to_user = bot.send_message(message.chat.id, request_answer, reply_markup=markup)
         bot.register_next_step_handler(answer_to_user, words_test_choice)
     elif message.text == 'Tests':
         bot.send_message(message.chat.id, "This option is currently unavailable. 3", reply_markup=markup)
@@ -208,12 +60,12 @@ def initial_choice(message):
 @bot.message_handler(content_types=["text"])
 def words_test_choice(message):
     markup = ReplyKeyboardMarkup(row_width=2)
-    markup.add("Once again")
-    markup.add("End")
+    markup.add('Once again')
+    markup.add('Give up')
     if message.text.lower() == global_random_answer.lower():
         bot.send_message(message.chat.id, "You are right", reply_markup=markup)
     else:
-        bot.send_message(message.chat.id, "No, bro, are you kidding?", reply_markup=markup)
+        bot.send_message(message.chat.id, f"No, bro, are you kidding?{global_random_answer}", reply_markup=markup)
 
 
 # @bot.message_handler(func=lambda message: message.text in dict_list)
