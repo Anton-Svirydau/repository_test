9d528685eebfab79c7c577fd323773e1299818c2
Anton Svirydau <unskillton@gmail.com>
2024-12-13 16:22:20 +0300

    Update test_some.py

--- tg_bot/pythonProject1/test_some.py
+++ tg_bot/pythonProject1/test_some.py
@@ -1,46 +1,46 @@
-from eng_dict import words_test
-from eng_dict import test_word
-from eng_dict import world_test_list
-from eng_dict import word_list_test
-from eng_dict import x_random
-from eng_dict import random_words_choose
+# from eng_dict import words_test
+# from eng_dict import test_word
+# from eng_dict import world_test_list
+# from eng_dict import word_list_test
+# from eng_dict import x_random
+# from eng_dict import random_words_choose
 from googletrans import Translator
 
-words_test()
+# words_test()
+#
+# abc, bcd = test_word()
+#
+# print(abc, bcd)
 
-abc, bcd = test_word()
-
-print(abc, bcd)
-
-some_list = [
-    ["apple", "banana", "grape"],
-    ["pen", "pencil", "marker"],
-    ["one", "two", "apple"]
-]
-
-a = 0
-
-some_word = input()
-
-for some in some_list:
-    if some_word in some:
-        print("Yes")
-        a += 1
-        break
-
-if a == 0:
-    print("No")
-
-world_test_list()
-
-some_word, some_list = word_list_test()
-print(some_word, some_list)
-
-x_random(10)
-
-a, b = random_words_choose(3)
-print(a)
-print(b)
+# some_list = [
+#     ["apple", "banana", "grape"],
+#     ["pen", "pencil", "marker"],
+#     ["one", "two", "apple"]
+# ]
+#
+# a = 0
+#
+# some_word = input()
+#
+# for some in some_list:
+#     if some_word in some:
+#         print("Yes")
+#         a += 1
+#         break
+#
+# if a == 0:
+#     print("No")
+#
+# world_test_list()
+#
+# some_word, some_list = word_list_test()
+# print(some_word, some_list)
+#
+# x_random(10)
+#
+# a, b = random_words_choose(3)
+# print(a)
+# print(b)
 
 translator = Translator()
 
