3ca960388f7e1b29585d1e77c1c6001e572d6c68
Anton Svirydau <unskillton@gmail.com>
2025-01-09 18:44:56 +0300

    Update test_some.py

--- tg_bot/pythonProject1/test_some.py
+++ tg_bot/pythonProject1/test_some.py
@@ -44,28 +44,36 @@ from timeit import default_timer as timer
 # print(b)
 
 
-translator = Translator()
+# translator = Translator()
+#
+# src = 'en'
+# dest = 'ru'
+#
+# text = "space"
+# translate_1 = input()
+#
+# start = timer()
+#
+# translated_text = translator.translate(translate_1, src=src, dest=dest).text
+#
+# print(translated_text)
+#
+# end = timer()
+# print(f"Время выполнения: {end - start:.5f} секунд")
+#
+#
+# def prnt():
+#     some = 1
+#     if some == 1:
+#         print("some = 1")
+#     else:
+#         print("something wrong ;(")
+#     return some
 
-src = 'en'
-dest = 'ru'
+s = input()
+a = []
+max_len = 0
 
-text = "space"
-translate_1 = input()
-
-start = timer()
-
-translated_text = translator.translate(translate_1, src=src, dest=dest).text
-
-print(translated_text)
-
-end = timer()
-print(f"Время выполнения: {end - start:.5f} секунд")
-
-
-def prnt():
-    some = 1
-    if some == 1:
-        print("some = 1")
-    else:
-        print("something wrong ;(")
-    return some
+for i in range(len(s)):
+    for j in range(len(s)):
+        a += s
