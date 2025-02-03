f2305fb450ca00de4b26840f3a2dbdeb2e9b58c2
Anton Svirydau <unskillton@gmail.com>
2025-02-03 17:53:00 +0300

    Update test_some.py

--- tg_bot/pythonProject1/test_some.py
+++ tg_bot/pythonProject1/test_some.py
@@ -69,28 +69,3 @@ from timeit import default_timer as timer
 #     else:
 #         print("something wrong ;(")
 #     return some
-
-s = input()
-a = []
-max_len = 0
-p = 0
-d = 0
-
-for i in range(len(s)):
-    if s[i] in s[i+1:]:
-        p = s.find(s[i], i+1, len(s))
-        a.append(s[i:p])
-        p = 0
-    else:
-        a.append(s[i:])
-        continue
-
-# for j in a:
-#     for k in j:
-#         if j.count(k) > 1:
-#             d = 1
-#     if d == 1:
-#         a.remove(j)
-#     d = 0
-
-print(a)
